ccd7438e9f9716f0568b68573b8ec3142ebe452d
Michael Bestas <mkbestas@gmail.com>
2020-10-30 20:06:04 +0200

    Integrate carriersettings extractor into factory image extraction
    * Only extract APNs for now
    
    Change-Id: Ie685d5ed3293cbf6b3ab2d1b245b34bdedbdd07d

--- scripts/carriersettings-extractor/download_carrier_list.sh
+++ scripts/carriersettings-extractor/download_carrier_list.sh
@@ -2,6 +2,38 @@
 
 set -euo pipefail
 
-curl -fS \
-  'https://android.googlesource.com/platform/packages/providers/TelephonyProvider/+/master/assets/latest_carrier_id/carrier_list.pb?format=TEXT' | \
-  base64 --decode > carrier_list.pb
+usage() {
+cat <<_EOF
+  Usage: $(basename "$0") [options]
+    OPTIONS:
+      -o|--output  : Path to save carrier list
+_EOF
+  abort 1
+}
+
+OUTPUT_DIR=""
+
+while [[ $# -gt 0 ]]
+do
+  arg="$1"
+  case $arg in
+    -o|--output)
+      OUTPUT_DIR=$(echo "$2" | sed 's:/*$::')
+      shift
+      ;;
+    *)
+      echo "[-] Invalid argument '$1'"
+      usage
+      ;;
+  esac
+  shift
+done
+
+if [[ "$OUTPUT_DIR" == "" || ! -d "$OUTPUT_DIR" ]]; then
+  echo "[-] Output directory not found"
+  usage
+fi
+
+url='https://android.googlesource.com/platform/packages/providers/TelephonyProvider/+/master/assets/latest_carrier_id/carrier_list.pb?format=TEXT'
+echo "[*] Downloading carrier list from '$url'"
+curl -fS "$url" | base64 --decode > "$OUTPUT_DIR"/carrier_list.pb
