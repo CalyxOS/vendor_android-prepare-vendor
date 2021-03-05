9b1726819fbb0f3ab21d75a9e339f965042994b2
Chirayu Desai <chirayudesai1@gmail.com>
2021-03-06 02:13:58 +0530

    carriersettings-extractor: Exclude some values from vendor.xml
    * This will make it work with the naked config
    * We can have two different XMLs if needed, where one
      could include this config
    * Looked for any 'com.google.android.' or Sprint/Verizon
      packages and removed those
    
    Change-Id: I3d68c995f855c13231946a5796091936479906e9

--- scripts/carriersettings-extractor/carriersettings_extractor.py
+++ scripts/carriersettings-extractor/carriersettings_extractor.py
@@ -53,6 +53,15 @@ def main():
     apns_folder = args.apns
     vendor_folder = args.vendor
 
+    # Anything where the value is a package name
+    unwanted_configs = ["carrier_app_wake_signal_config",
+                        "carrier_settings_activity_component_name_string",
+                        "carrier_setup_app_string",
+                        "config_ims_package_override_string",
+                        "enable_apps_string_array",
+                        "gps.nfw_proxy_apps",
+                        "wfc_emergency_address_carrier_app_string"]
+
     carrier_id_list = CarrierIdList()
     carrier_attribute_map = {}
     with open(os.path.join(carrier_list_folder, 'carrier_list.pb'), 'rb') as pb:
@@ -218,6 +227,8 @@ def main():
                         getattr(entry.carrierId, field),
                     )
             for config in setting.configs.config:
+                if config.key in unwanted_configs:
+                    continue
                 value_type = config.WhichOneof('value')
                 if value_type == 'textValue':
                     carrier_config_subelement = ET.SubElement(
