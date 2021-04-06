57453503e7dab20a9e5e29eb352a16227682978f
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-06 21:10:40 +0530

    Put overlays directly under vendor/google_devices
    We have this or subidrs as a git repo, so put the overlay in here
    directly rather than having a new vendor_overlay
    
    Change-Id: I930027d7deb88f4ab75b29bc2977fd4e44b573a8

--- scripts/generate-vendor.sh
+++ scripts/generate-vendor.sh
@@ -1302,7 +1302,7 @@ if [[ "$DEVICE" != "$DEVICE_FAMILY" ]]; then
 fi
 
 # Prepare vendor overlays output directory structure
-readonly REL_VENDOR_OVERLAY="vendor_overlay/$VENDOR_DIR/$DEVICE/overlay"
+readonly REL_VENDOR_OVERLAY="vendor/$VENDOR_DIR/$DEVICE/overlay"
 OUTPUT_VENDOR_OVERLAY="$OUTPUT_DIR/$REL_VENDOR_OVERLAY"
 if [ -d "$OUTPUT_VENDOR_OVERLAY" ]; then
   rm -rf "${OUTPUT_VENDOR_OVERLAY:?}"/*
@@ -1453,12 +1453,12 @@ if [[ "$AOSP_ROOT" != "" ]]; then
 
   # Vendor overlays are always under separate directories so it's safe to force update
   if [[ "$OVERLAYS_DIR" != "" ]]; then
-    mkdir -p "$AOSP_ROOT/vendor_overlay/$VENDOR_DIR"
-    rsync -arz --delete "$OUTPUT_DIR/vendor_overlay/$VENDOR_DIR/" "$AOSP_ROOT/vendor_overlay/$VENDOR_DIR/" || {
+    mkdir -p "$AOSP_ROOT/vendor/$VENDOR_DIR"
+    rsync -arz --delete "$OUTPUT_DIR/vendor/$VENDOR_DIR/" "$AOSP_ROOT/vendor/$VENDOR_DIR/" || {
       echo "[-] rsync failed"
       abort 1
     }
-    echo "[*] Vendor overlays copied to '$AOSP_ROOT/vendor_overlay/$VENDOR_DIR'"
+    echo "[*] Vendor overlays copied to '$AOSP_ROOT/vendor/$VENDOR_DIR/$DEVICE/overlay'"
   fi
 fi
 
